3696f68161d7f7a150ac81be49b53bb0e86f038a
NotACat1 <notacat.mail@gmail.com>
2023-06-26 22:44:05 +0400

    build: Решение задачи
    Функция intersection(arr1, arr2). Она должна принимать на вход два массива целых чисел. Функция должна вернуть новый массив чисел, содержащихся в обоих исходных массивах.

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -8,10 +8,13 @@
 */
 
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+  const rez = [];
+  arr1.forEach(item => { if (arr2.includes(item) && !rez.includes(item)) rez.push(item); });
+  return rez;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(intersection([1, 5, 4, 2], [8, 91, 4, 1, 3])); // [4, 1]
 console.log(intersection([1, 5, 4, 2], [7, 12])); // []
+console.log(intersection([1, 1, 5, 4, 2], [8, 91, 4, 1, 1, 3]));
