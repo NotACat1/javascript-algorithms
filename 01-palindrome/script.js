6e87c75ce183ada1bfdd2bbef9d955a84bc41f7a
NotACat1 <notacat.mail@gmail.com>
2023-06-27 14:59:40 +0400

    style: Исправлена табуляция

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -14,15 +14,15 @@
 */
 
 function palindrome(str) {
-    const checkStr = str.toLowerCase().replace(/[^a-zа-я]+/g, ''); // перевод в нижний регистр + избавление в строке от пробелов и знаков припинания
-    let leftBorder = 0; // индекс левого символа
-    let rightBorder = checkStr.length - 1; // индекс правого символа
-    while (leftBorder <= rightBorder) {
-      if (checkStr[leftBorder] != checkStr[rightBorder]) return false;
-      leftBorder++;
-      rightBorder--;
-    }
-    return true;
+  const checkStr = str.toLowerCase().replace(/[^a-zа-я]+/g, ''); // перевод в нижний регистр + избавление в строке от пробелов и знаков припинания
+  let leftBorder = 0; // индекс левого символа
+  let rightBorder = checkStr.length - 1; // индекс правого символа
+  while (leftBorder <= rightBorder) {
+    if (checkStr[leftBorder] != checkStr[rightBorder]) return false;
+    leftBorder++;
+    rightBorder--;
+  }
+  return true;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
